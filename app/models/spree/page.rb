c8acf3769e6d064e79a53f2b6cf8e9ddab1a197b
Steven Spriggs <steven.spriggs@gmail.com>
2012-05-24 18:17:17 -0400

    make model attributes accessible to updating

--- app/models/spree/page.rb
+++ app/models/spree/page.rb
@@ -2,6 +2,8 @@ module Spree
   class Page < ActiveRecord::Base
     validates :title, :presence => true
     validates :permalink, :uniqueness => true
+    
+    attr_accessible :title, :meta_keywords, :meta_description, :body, :permalink, :published
   
     scope :published, where(:published => true)
 
